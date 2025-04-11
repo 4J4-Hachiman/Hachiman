50d8aebd46db8b958f4b41b44cf10207e9852920
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2025-04-11 08:55:47 -0400

    Gestion des chargements de scènes, cinématiques et assemblage du menu crédit
    Ajout d'hyperliens dans le menu crédit, Fonctionnement du bouton quitter, chargement des scènes appelé en asynchrone pour une ouverture de scène complètement chargée, transition fluide entre les scènes, utilisation de timeline

--- Assets/Scripts/GestionMedia/GestionUI/SceneActiveManager.cs
+++ Assets/Scripts/GestionMedia/GestionUI/SceneActiveManager.cs
@@ -1,19 +1,73 @@
 /*  
- *  Fonctionnement et utilité générale du script
+ *  Fonctionnement et utilitï¿½ gï¿½nï¿½rale du script
 
 [Gestionnaire]
 
-    Fonction pour les changements de scènes
-        Par : Malaïka Abevi
-        Dernière modification : 29/03/2025
+    Fonction pour les changements de scï¿½nes
+        Par : Malaï¿½ka Abevi
+        Derniï¿½re modification : 10/04/2025
 */
+using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneActiveManager : MonoBehaviour
 {
-    public void ChangementScene(string nomScene)
+    public static SceneActiveManager instance;
+
+    void Start()
     {
-        SceneManager.LoadScene(nomScene);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
+
+    // public void ChangementScene(string nomScene)
+    // {
+    //     // SceneManager.LoadScene(nomScene);
+    //     // Use a coroutine to load the Scene in the background
+    //     // StartCoroutine(LoadYourAsyncScene(nomScene));
+    // }
+
+    public async void ChargerScene(string nomScene){
+        var scene = SceneManager.LoadSceneAsync(nomScene);
+        scene.allowSceneActivation = false;
+
+        //Instructions nÃ©cÃ©ssaires blablabla
+        if(!scene.isDone){
+            
+        }
+        do{
+            Debug.Log("Chargement...");
+        }while(scene.progress < 0.9f);
+
+        Debug.Log("La scÃ¨ne est chargÃ©e");
+        
+        await Task.Delay(15000);
+
+        scene.allowSceneActivation = true;
+
+    }
+    // IEnumerator LoadYourAsyncScene(string nomScene)
+    // {
+    //     // The Application loads the Scene in the background as the current Scene runs.
+    //     // This is particularly good for creating loading screens.
+    //     // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+    //     // a sceneBuildIndex of 1 as shown in Build Settings.
+
+    //     AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scene2");
+
+    //     // Wait until the asynchronous scene fully loads
+    //     while (!asyncLoad.isDone)
+    //     {
+    //         yield return null;
+    //     }
+    // }
 }
