ad00747d72b0cc08fe21f200e5a6b98db8e1632a
Yanis Oulmane <e2073117@cmaisonneuve.qc.ca>
2025-04-25 09:48:59 -0400

    Masarai
    ### Mise à jour du prefab :
    - Separation du mesh et des animations de Masarai pour meilleeur gestion des deux elements.
    
    ### Mise a jour des textures :
    - Changement des textures du couleur et des normal.

--- Assets/Scripts/Char_Ennemies/1-Boss/StateMachine/States/MasaraiStateWalk.cs
+++ Assets/Scripts/Char_Ennemies/1-Boss/StateMachine/States/MasaraiStateWalk.cs
@@ -4,23 +4,15 @@ public class MasaraiStateWalk : MasaraiBaseState
 {
     public MasaraiStateWalk(MasaraiStateMachine masaraiSM, MasaraiMain main) : base(masaraiSM, main) { }
 
-    public override void StateStart(bool init = false)
-    {
-        
-    }
+    public override void StateStart(bool init = false) { }
 
-    public override void StateExit()
-    {
-        
-    }
+    public override void StateExit() { }
 
     public override void StateFixedUpdate()
     {
+        Debug.Log("Masarai State FixedUpdate performed.");
         main.SetNavAgentDestination(main.Player.position);
     }
-
-    public override void StateUpdate()
-    {
-        
-    }
+    
+    public override void StateUpdate() { }
 }
