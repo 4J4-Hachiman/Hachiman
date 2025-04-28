6138cc471e80de43aa07660e8239c09f5cdbda8d
SebastienGithub <hikup.github@gmail.com>
2025-04-28 14:26:10 -0400

    Sparks finished

--- Assets/Scripts/Char_Joueur/JoueursControl1.cs
+++ Assets/Scripts/Char_Joueur/JoueursControl1.cs
@@ -39,6 +39,7 @@ public class JoueursControl1 : MonoBehaviour
     [SerializeField] private LayerMask enemyLayer;
     public BanqueAudio banqueAudio;
     public ControlesVieMana uiVieMana;
+    public ParticleSystem sparksBlockEffect;
 
     /* -------------------- VARIABLES MOUVEMENT -------------------- */
     [Header("Mouvement et saut")]
@@ -1107,6 +1108,8 @@ public class JoueursControl1 : MonoBehaviour
                 {
                     Invoke("NotHit", 0.33f);
                     animator.SetBool("Hit", true);
+                    ParticleSystem sparksInt = Instantiate(sparksBlockEffect, katana.transform.position, katana.transform.rotation);
+                    Destroy(sparksInt.gameObject, 2f);
                     activeKatana.GetComponents<AudioSource>()[2].PlayOneShot(banqueAudio.sSwordClash2);
                 }
             }
